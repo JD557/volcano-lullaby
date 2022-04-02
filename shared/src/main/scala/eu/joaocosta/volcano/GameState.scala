c63cc105516a8f14b5eb70367bec347151d28cc9
Joao Costa <jdpc557@gmail.com>
2022-04-02 10:38:58 +0200

    Add basic collision detection

--- shared/src/main/scala/eu/joaocosta/volcano/GameState.scala
+++ shared/src/main/scala/eu/joaocosta/volcano/GameState.scala
@@ -5,15 +5,34 @@ import eu.joaocosta.minart.graphics._
 import eu.joaocosta.minart.graphics.image._
 
 final case class GameState(charX: Int, charY: Int, level: Level) {
-  val charTileX = charX / 16
-  val charTileY = charY / 16
+  val applyGravity = movePlayer(0, 1)
 
-  val applyGravity = 
-    if (level.tiles(charTileY + 2)(charTileX) == 0) movePlayer(0, 1)
+  def movePlayer(dx: Int, dy: Int): GameState =
+    if (dx != 0) {
+      val newX = charX + dx 
+      lazy val tiles = Set(
+        level.tiles(charY / 16)(newX / 16),
+        level.tiles((charY + 31) / 16)((newX + 15) / 16),
+        level.tiles(charY / 16)(newX / 16),
+        level.tiles((charY + 31) / 16)((newX + 15) / 16)
+      )
+      if (newX < 0 || newX + 15 > level.width * 16 || tiles != Set(0))
+        movePlayer(0, dy)
+      else copy(charX = newX).movePlayer(0, dy)
+    }
+    else if (dy != 0) {
+      val newY = charY + dy
+      lazy val tiles = Set(
+        level.tiles(newY / 16)(charX / 16),
+        level.tiles((newY + 31) / 16)((charX + 15) / 16),
+        level.tiles(newY / 16)(charX / 16),
+        level.tiles((newY + 31) / 16)((charX + 15) / 16)
+      )
+      if (newY < 0 || newY + 31 > level.height * 16 || tiles != Set(0))
+        movePlayer(0, 0)
+      else copy(charY = newY)
+    }
     else this
-
-  def movePlayer(dx: Int, dy: Int) =
-    copy(charX = charX + dx, charY = charY + dy)
 
   def processInput(key: KeyboardInput) = 
     if (key.isDown(KeyboardInput.Key.Right)) movePlayer(1, 0)
